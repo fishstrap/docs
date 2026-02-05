7c0326cae85c489cf354bfe2b47c41cf38376d2b
Alexa <67791946+GoingCrazyDude@users.noreply.github.com>
2026-02-06 01:31:19 +0530

    docs: add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
 	integrations: [
         starlight({
             title: 'Fishstrap',
+            favicon: '/favicon.png',
             logo: {
                 src: './src/assets/favicon.webp',
                 replacesTitle: true,
